11c1af0b38c15589de83c0b9895aaa3a13b94865
Casey Megginson <tenncasey@gmail.com>
2014-11-30 01:41:37 -0600

    figured it out

--- perfectnumber.c
+++ perfectnumber.c
@@ -7,12 +7,13 @@ bool is_prime(long long int x);
 int main() {
 	long long int p, mersenne, perfect;
 
-	for(p = 3; 1; p += 2) {
-		mersenne = pow(2, p) - 1;
+	for(p = 2; 1; p++) {
+		mersenne = powl(2, p) - 1;
 
 		if(is_prime(mersenne)) {
-			perfect = (mersenne *(mersenne + 1)) / 2;
-			printf("%i", mersenne);
+			perfect = powl(2, p-1) * mersenne;
+			printf("%s %i\n", "Mersenne Prime: ", mersenne);
+			printf("%s %i\n", "Perfect Number: ", perfect);
 		}
 	}
 	return 0;
@@ -21,6 +22,10 @@ int main() {
 bool is_prime(long long int x) {
 	long long int root = (long long) sqrtl(x);
 	long long int i;
+
+	if(x == 2) {
+		return 1;
+	}
 
 	for(i = 3; i < root; i += 2) {
 		if(x % i ==0) {
